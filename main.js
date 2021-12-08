03e12bc1c913e8077a87fc6a3dc3e405131d0640
kritik jain <kritikjain9@gmail.com>
2021-12-08 14:22:36 +0530

    Initial commit

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 let fs = require("fs");
 let path = require('path');
 
-let userInput = process.argv.slice(2);
-// console.log(userInput);
-let commandGiven = userInput.filter(inp => inp.length == 2);
+let userInput = process.argv.slice(3);  
+console.log(userInput);
 
-switch(commandGiven){
-        case "filepath":
-          // code block
-          break;
-        case "":
-          // code block
-          break;
-        default:
-          // isme only "filepath waale ka result daal dena"
+let lengthCMD = userInput.length;
+if(lengthCMD == 1){
+  
 }
-console.log(commandGiven);
+
+// let commandGiven = userInput.filter(inp => inp.length == 2);
+
+// switch(commandGiven){
+//         case "filepath":
+//           // code blockf
+//           break;
+//         case "":
+//           // code block
+//           break;
+//         default:
+//           // isme only "filepath waale ka result daal dena"
+// }
+// console.log(commandGiven);
